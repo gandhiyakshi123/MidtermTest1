7301f3d48c4d38310d0570dd265f45483664c4eb
Yakshi Gandhi <200482015@student.georgianc.on.ca>
2022-06-23 10:54:47 -0400

    Question 3 complete

--- src/main/java/com/example/midtermtest1/StudentTableViewController.java
+++ src/main/java/com/example/midtermtest1/StudentTableViewController.java
@@ -1,9 +1,14 @@
 package com.example.midtermtest1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.PropertyValueFactory;
 
-public class StudentTableViewController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StudentTableViewController implements Initializable {
 
     @FXML
     private TableColumn<Student, Integer> avgGradeColumn;
@@ -24,9 +29,26 @@ public class StudentTableViewController {
     private TableColumn<Student, String> provinceColumn;
 
     @FXML
-    private TableColumn<Student, Integer> studentIDColumn;
+    private TableColumn<Student, Integer> studentNumColumn;
 
     @FXML
     private TableColumn<Student, String> telephoneColumn;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        // configure the columns to know where to get the information from the Student object
+        studentNumColumn.setCellValueFactory(new PropertyValueFactory<>("studentNum"));
+        firstNameColumn.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        lastNameColumn.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        genderColumn.setCellValueFactory(new PropertyValueFactory<>("gender"));
+        telephoneColumn.setCellValueFactory(new PropertyValueFactory<>("telephone"));
+        provinceColumn.setCellValueFactory(new PropertyValueFactory<>("province"));
+        majorColumn.setCellValueFactory(new PropertyValueFactory<>("majorCode"));
+        avgGradeColumn.setCellValueFactory(new PropertyValueFactory<>("avgGrade"));
+
+        // load the students into the tableView
+       // StudentTableViewController.get().addAll(DBUtility.getStudentFromDB());
+
+    }
 }
